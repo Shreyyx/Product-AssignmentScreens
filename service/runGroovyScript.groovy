724b9a825e79e0f93d1382857c99d6d4950bd199
Shreyyx <shreyasingh210501@acropolis.in>
2024-12-24 15:32:19 +0530

    services added

---
+++ service/runGroovyScript.groovy
@@ -0,0 +1,8 @@
+//def moquiTraining = ec.entity.makeValue("MoquiTraining")
+//moquiTraining.setFields(context, true, null, null)
+//if (!moquiTraining.trainingId) {
+//    moquiTraining.getSequencedIdPrimary()
+//}
+//moquiTraining.create()
+//return context
+//
