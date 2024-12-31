ebb980017326c4211474fa1f70ce1f89ffa272e8
Shreyyx <shreyasingh210501@acropolis.in>
2024-12-31 18:45:45 +0530

    Api related changes

--- service/runGroovyScript.groovy
+++ service/runGroovyScript.groovy
@@ -1,5 +1,5 @@
-def training = ec.entity.makeValue("MoquiTraining")
-training.setFields(context, true, null, null)
-training.setSequencedIdPrimary()
-training.create()
-context.trainingId = training.trainingId
+def groovy_training = ec.entity.makeValue("MoquiTraining")
+groovy_training.setFields(context, true, null, null)
+groovy_training.setSequencedIdPrimary()
+groovy_training.create()
+context.trainingId = groovy_training.trainingId
